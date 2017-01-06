eb91eb1731206efcf53b3dc7a99c006447d6a962
oreqizer <oreqizer@gmail.com>
2017-01-06 20:59:31 +0100

    build: Cleanup, adjust webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,40 @@
 const path = require('path');
+const webpack = require('webpack');
+const AssetsPlugin = require('assets-webpack-plugin');
 
 
-module.exports = {
-  entry: './src/client/index.ts',
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+
+const config = {
+  entry: {
+    bundle: './src/client/index.ts',
+    vendor: ['inferno', 'inferno-redux', 'redux'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist/static'),
-    filename: 'bundle.[hash].js',
+    filename: '[name].[hash].js',
   },
   module: {
     rules: [{
       test: /tsx?$/,
-      use: [{
-        loader: 'babel-loader',
-      }, {
-        loader: 'ts-loader',
-      }],
+      use: ['babel-loader', 'ts-loader'],
     }],
   },
+  plugins: [
+    new AssetsPlugin({ path: 'dist', filename: 'assets.json' }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify({ NODE_ENV: PRODUCTION ? 'production' : 'dev' }),
+    }),
+  ],
 };
+
+if (PRODUCTION) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = config;
