3907054c2de668006dac1278ff455fb74b037bca
oreqizer <oreqizer@gmail.com>
2017-01-07 15:45:35 +0100

    general: Simplify TS 2

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,58 @@ const Assets = require('assets-webpack-plugin');
 const prod = process.env.NODE_ENV === 'production';
 
 
-const config = {
+const rules = [{
+  test: /tsx?$/,
+  use: [{
+    loader: 'babel-loader',
+    options: {
+      presets: [['es2015', { modules: false }], 'stage-3'],
+      plugins: ['inferno'],
+    },
+  }, {
+    loader: 'ts-loader',
+  }],
+}, {
+  test: /\.css$/,
+  loader: ExtractText.extract({
+    fallbackLoader: 'style-loader',
+    loader: 'css-loader?modules',
+  }),
+}];
+
+
+const plugins = [
+  new webpack.LoaderOptionsPlugin({
+    debug: !prod,
+    options: {
+      resolve: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+  }),
+  new Assets({
+    path: 'dist',
+    filename: 'assets.json',
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: Infinity,
+  }),
+  new webpack.DefinePlugin({
+    'process.env': JSON.stringify({ NODE_ENV: prod ? 'production' : 'dev' }),
+  }),
+];
+
+if (prod) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+} else {
+  plugins.push(new webpack.SourceMapDevToolPlugin({
+    exclude: /vendor/,
+  }));
+}
+
+
+module.exports = {
   entry: {
     bundle: './src/client/index.ts',
     vendor: ['inferno'],
@@ -17,49 +68,7 @@ const config = {
     filename: 'static/[name].[hash].js',
   },
   module: {
-    rules: [{
-      test: /tsx?$/,
-      use: ['babel-loader', 'ts-loader'],
-    }, {
-      test: /\.css$/,
-      loader: ExtractText.extract({
-        fallbackLoader: 'style-loader',
-        loader: 'css-loader?modules',
-      }),
-    }, {
-      test: /\.html$/,
-      loader: 'html-loader',
-    }],
+    rules,
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      debug: !prod,
-      options: {
-        resolve: {
-          extensions: ['.ts', '.tsx'],
-        },
-      },
-    }),
-    new Assets({
-      path: 'dist',
-      filename: 'assets.json',
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify({ NODE_ENV: prod ? 'production' : 'dev' }),
-    }),
-  ],
+  plugins,
 };
-
-if (prod) {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-} else {
-  config.plugins.push(new webpack.SourceMapDevToolPlugin({
-    exclude: /vendor/,
-  }));
-}
-
-module.exports = config;
