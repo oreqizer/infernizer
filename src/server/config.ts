17ddc2e6608a91f23a616fa7950b133285517282
oreqizer <oreqizer@gmail.com>
2017-03-06 17:26:29 +0100

    refactor(config): Better assets.json handling

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,28 +2,27 @@ import fs from 'fs';
 import path from 'path';
 
 
+export const production = process.env.NODE_ENV === 'production';
+
+export const port = Number(process.env.PORT || 3000);
+
+
 type Assets = {
   bundle: { js: string, css?: string },
   vendor: { js: string, css?: string },
 };
 
-
 function loadAssets(): Assets {
-  try {
+  if (production) {
     const raw = fs.readFileSync(path.join(__dirname, '../assets.json'));
 
     return JSON.parse(String(raw));
-  } catch (err) {
-    // No assets.json - .css are inline on development
-    return {
-      bundle: { js: 'bundle.js' },
-      vendor: { js: 'vendor.js' },
-    };
   }
+
+  return {
+    bundle: { js: 'bundle.js' },
+    vendor: { js: 'vendor.js' },
+  };
 }
 
 export const assets = loadAssets();
-
-export const production = process.env.NODE_ENV === 'production';
-
-export const port = Number(process.env.PORT || 3000);
