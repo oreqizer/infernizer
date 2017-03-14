bdfdc70c0a2d61ba1685790def48bd94865eed06
oreqizer <oreqizer@gmail.com>
2017-03-14 16:05:55 +0100

    feat(types): Add flow definition folder

--- src/server/app.jsx
+++ src/server/app.jsx
@@ -1,12 +1,14 @@
 /* @flow */
+/* eslint-disable no-param-reassign */
 import React from 'react';
 import ReactServer from 'react-dom/server';
+import type { Context } from 'koa';
 
 import Root from '../client/modules/Root';
 import Html from './markup/Html';
 
 
-async function app(ctx, next) {
+async function app(ctx: Context) {
   const root = ReactServer.renderToString(<Root />);
 
   ctx.body = ReactServer.renderToStaticMarkup(<Html root={root} />);
